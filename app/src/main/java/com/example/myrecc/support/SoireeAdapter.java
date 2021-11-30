c76d0e9defe05e6c667d6d080002dd12ca8067e6
Cyril Bretaud <cyril-bretaud@orange.fr>
2021-11-30 09:57:19 +0100

    modification
    modification SoireeAdapter et SoireeViewHolder

--- app/src/main/java/com/example/myrecc/support/SoireeAdapter.java
+++ app/src/main/java/com/example/myrecc/support/SoireeAdapter.java
@@ -1,4 +1,25 @@
 package com.example.myrecc.support;
 
-public class SoireeAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SoireeAdapter extends RecyclerView.Adapter<SoireeViewHolder> {
+
+    @NonNull
+    @Override
+    public SoireeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SoireeViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/com/example/myrecc/support/SoireeViewHolder.java
+++ app/src/main/java/com/example/myrecc/support/SoireeViewHolder.java
@@ -1,4 +1,17 @@
 package com.example.myrecc.support;
 
-public class SoireeViewHolder {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SoireeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public SoireeViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
